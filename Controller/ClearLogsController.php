9b383a1e9b6e462809d99002289274cab165625f
Lukas Marks <lukas.marks@lumax-web.de>
2015-05-15 13:56:55 +0200

    Changelog, License and Version

--- Controller/ClearLogsController.php
+++ Controller/ClearLogsController.php
@@ -1,13 +1,17 @@
 <?php
 /**
  * ClearLog
+ * Copyright (c) Lukas Marks (http://lumax-web.de/)
  *
- * @category Controller
- * @package  ClearLog
- * @version  2.x
- * @author   Lukas Marks <info@lumax-web.de>
- * @link     http://www.lumax-web.de/
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Lukas Marks (http://lumax-web.de/)
+ * @since         0.1
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
+
 App::uses('ClearLogsAppController', 'ClearLog.Controller');
 
 class ClearLogsController extends ClearLogsAppController {
@@ -15,6 +19,10 @@ class ClearLogsController extends ClearLogsAppController {
 	public function admin_clear() {
 		$this->ClearLog->delete();
 		$this->Session->setFlash(__d('clear_log', 'Log files has been deleted successfully.'), 'flash', array('class' => 'success'));
-		$this->redirect(array('plugin' => 'settings', 'controller' => 'settings', 'action' => 'dashboard'));
+		$this->redirect(array(
+			'plugin' => 'settings',
+			'controller' => 'settings',
+			'action' => 'dashboard',
+		));
 	}
 }
--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-Croogo_ClearLog
-=================
+# ClearLog
 
-ClearLog Plugin for CakePHP & Croogo 2.x Versions.
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](LICENSE.txt)
+
+Croogo CMS ClearLog plugin for delete log files in the tmp/logs folder.
+
+## Issues
+
+Please report any issues you have with the plugin to the [issue tracker](http://github.com/LuMAXW3B/Croogo_ClearLog/issues) on github.
+
+## License
+
+Asset Compress is offered under an [MIT license](http://www.opensource.org/licenses/mit-license.php).
+
+## Copyright
+
+2015 Lukas Marks (http://lumax-web.de/)
+
+### Authors
+
+See the [github contributors list](http://github.com/LuMAXW3B/Croogo_ClearLog/graphs/contributors)
+
+### Changelog
+
+See CHANGELOG for changes only available on `master`. See
+[github releases](http://github.com/LuMAXW3B/Croogo_ClearLog/releases) for changelogs on previous releases.
