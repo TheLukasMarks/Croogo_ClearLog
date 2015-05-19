7b733f0433e02709ea564b915919c1744498c08b
Lukas Marks <lukas.marks@lumax-web.de>
2015-05-19 12:15:36 +0200

    Refactoring

--- Controller/ClearLogsController.php
+++ Controller/ClearLogsController.php
@@ -11,14 +11,31 @@
  * @since         0.1
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
 App::uses('ClearLogsAppController', 'ClearLog.Controller');
 
 class ClearLogsController extends ClearLogsAppController {
+	
+	/**
+	 * Models used by the Controller
+	 *
+	 * @var array
+	 * @access public
+	 */ 
 	public $uses = array('ClearLog.ClearLog');
+	
+	/**
+	 * Admin clear
+	 *
+	 * @return void
+	 * @access public
+	 */
 	public function admin_clear() {
-		$this->ClearLog->delete();
-		$this->Session->setFlash(__d('clear_log', 'Log files has been deleted successfully.'), 'flash', array('class' => 'success'));
+		if ($this->{$this->modelClass}->delete()) {
+			$this->Session->setFlash(__d('clear_log', 'Log files has been deleted successfully.'), 'flash', array('class' => 'success'));
+			return $this->redirect(Configure::read('Croogo.dashboardUrl'));
+		}
+		
+		$this->Session->setFlash(__d('clear_log', 'An error occurred. Please, try again.'), 'flash', array('class' => 'error'));
 		return $this->redirect(Configure::read('Croogo.dashboardUrl'));
 	}
 }
--- Model/ClearLog.php
+++ Model/ClearLog.php
@@ -11,29 +11,83 @@
  * @since         0.1
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
 App::uses('ClearLogAppModel', 'ClearLog.Model');
 
 class ClearLog extends ClearLogAppModel {
 
+	/**
+	 * Model name
+	 *
+	 * @var string
+	 * @access public
+	 */
 	public $name = 'ClearLog';
+	
+	/**
+	 * Model table
+	 *
+	 * @var string
+	 * @access public
+	 */
 	public $useTable = false;
 
-	public function delete($path = null, $recursive = true) {
-		if (!$path) $path = TMP . 'logs' . DS;
+	/**
+	 * Removes record for given ID. If no ID is given, the current ID is used. Returns true on success.
+	 *
+	 * @param int|string $id ID of record to delete
+	 * @param bool $cascade Set to true to delete records that depend on this record
+	 * @return bool True on success
+	 * @triggers Model.beforeDelete $this, array($cascade)
+	 * @triggers Model.afterDelete $this
+	 * @link http://book.cakephp.org/2.0/en/models/deleting-data.html
+	 */
+	public function delete($id = null, $cascade = true) {
+		
+		/**
+		 * Constructor.
+		 *
+		 * @param string $path Path to folder
+		 * @param bool $create Create folder if not found
+		 * @param string|bool $mode Mode (CHMOD) to apply to created folder, false to ignore
+		 * @link http://book.cakephp.org/2.0/en/core-utility-libraries/file-folder.html#Folder
+		 */
+		$dir = new Folder(TMP . 'logs');
+		
+		/**
+		 * Returns an array of all matching files in current directory.
+		 *
+		 * @param string $regexpPattern Preg_match pattern (Defaults to: .*)
+		 * @param bool $sort Whether results should be sorted.
+		 * @return array Files that match given pattern
+		 * @link http://book.cakephp.org/2.0/en/core-utility-libraries/file-folder.html#Folder::find
+		 */
+		$files = $dir->find('.*', true);
 
-		$dirItems = scandir($path);
-		$ignore = array('.', '..');
-
-		foreach ($dirItems AS $dirItem) {
-			if (in_array($dirItem, $ignore)) continue;
-			if (is_dir($path . $dirItem) && $recursive) {
-				$this->delete($path . $dirItem . DS);
-			} elseif (substr($dirItem, 0, 5) == 'debug') {
-				unlink($path . $dirItem);
-			} elseif (substr($dirItem, 0, 5) == 'error') {
-				unlink($path . $dirItem);
+		foreach ($files as $file) {
+			
+			/**
+			 * Constructor
+			 *
+			 * @param string $path Path to file
+			 * @param boolean $create Create file if it does not exist (if true)
+			 * @param integer $mode Mode to apply to the folder holding the file
+			 * @link http://book.cakephp.org/2.0/en/core-utility-libraries/file-folder.html#File
+			 */
+			$file = new File($dir->pwd() . DS . $file);
+			
+			/**
+			 * Deletes the File.
+			 *
+			 * @return boolean Success
+			 * @link http://book.cakephp.org/2.0/en/core-utility-libraries/file-folder.html#File::delete
+			 */
+			if ($file->delete()) {
+				continue;
+			} else {
+				return false;
 			}
 		}
+		
+		return true;
 	}
 }
